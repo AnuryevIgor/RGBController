7131e013dd8836253161ca2c2d0094e60b7b355a
AnuryevIgor <anuryev.i.v@gmail.com>
2021-09-03 10:56:11 +0300

    final version

--- RGBController/ViewController.swift
+++ RGBController/ViewController.swift
@@ -24,25 +24,24 @@ class ViewController: UIViewController {
         
         colorView.layer.cornerRadius = 20
         
-        colorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )
-        redLabel.text = String(format: "%.2f", redSlider.value)
-        greenLabel.text = String(format: "%.2f", greenSlider.value)
-        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        setColor()
         
-        
+        rgbSlider()
     }
-    @IBAction func rgbSliders(_ sender: UISlider) {
+    @IBAction func rgbSliders(_: UISlider) {
+        setColor()
+        
+        rgbSlider()
+    }
+    private func setColor() {
         colorView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1
         )
+    }
+    private func rgbSlider() {
         redLabel.text = String(format: "%.2f", redSlider.value)
         greenLabel.text = String(format: "%.2f", greenSlider.value)
         blueLabel.text = String(format: "%.2f", blueSlider.value)
